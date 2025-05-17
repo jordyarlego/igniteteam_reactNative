dd917e90516b94cccca6a8930436092edbcb0c6d
jordyarlego <jordyarlego@gmail.com>
2025-05-17 18:13:51 -0300

    Adicionando fonts com expo fonts :fire:

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-
+import { ThemeProvider} from 'styled-components'
+import theme from './src/theme'
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
 import { Groups } from './src/Groups';
 export default function App() {
   return (
-    
+    <ThemeProvider theme={theme}>
       <Groups />
+    </ThemeProvider>
     
   );
 }
--- src/Groups/index.tsx
+++ src/Groups/index.tsx
@@ -1,11 +1,11 @@
 
 
-import { Container, Title } from './styles'
+import { Container } from './styles'
 
 export  function Groups() {
   return (
     <Container>
-      <Title>Olá sua puta</Title>
+      
     </Container>
   );
 }
