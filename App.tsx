697fe0b99ca792f802bec1750a0bdb584c413b6f
jordyarlego <jordyarlego@gmail.com>
2025-05-17 18:29:51 -0300

    LoadIndicator component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
+import { ActivityIndicator} from 'react-native'
 import { ThemeProvider} from 'styled-components'
 import theme from './src/theme'
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
-
+import { Loading } from './src/components/loading'
 import { Groups } from './src/Groups';
 export default function App() {
+
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {!fontsLoaded ? <Loading /> : <Loading />}
     </ThemeProvider>
     
   );
--- src/Groups/index.tsx
+++ src/Groups/index.tsx
@@ -1,11 +1,11 @@
 
 
-import { Container } from './styles'
+import { Container, LoadIndicator } from './styles'
 
 export  function Groups() {
   return (
     <Container>
-      
+      <LoadIndicator />
     </Container>
   );
 }
