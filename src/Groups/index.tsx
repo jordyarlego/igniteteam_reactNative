a851dc439241b412cf69c902437e1fd50316770b
jordyarlego <jordyarlego@gmail.com>
2025-05-19 15:57:03 -0300

    lista reutilizavel

--- src/Groups/index.tsx
+++ src/Groups/index.tsx
@@ -1,17 +1,33 @@
 
 import { Header } from '../components/Header';
+import { FlatList } from 'react-native';
 import { Destaques } from '../components/Destaques';
 import { GroupCard } from '../components/GroudCards';
 import { Container } from './styles'
+import { ListEmpty } from '../components/ListEmpty';
+import { useState } from 'react';
 
 export  function Groups() {
+
+  const [groups, setGroups] = useState<string[]>([]);
+
+
+
   return (
     <Container>
       <Header />
       <Destaques title="Grupos" subtitle="Jogue com sua turma" />
 
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <GroupCard title={item} icon="group" onPress={() => {}} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message="Nenhum grupo cadastrado.. 😢" />}
+      />
 
-      <GroupCard title="Turminha da pesada!" icon="group" onPress={() => {}} />
+
+      
     </Container>
   );
 }
