f33e4dc6cfe8945dffdba7cc4f64a4e2e51ed9ad
jordyarlego <jordyarlego@gmail.com>
2025-05-19 19:06:11 -0300

    Configuracao de navigation-stack

---
+++ src/Routes/app.routes.tsx
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Groups } from "../Screens/Groups";
+import { Players } from "../Screens/Players";
+import { NewGroup } from "../Screens/newGroup";
+const { Navigator, Screen } = createNativeStackNavigator();
+
+
+export function AppRoutes() {
+    return (
+        <Navigator>
+            <Screen name="groups" component={Groups} />
+            <Screen name="newGroup" component={NewGroup} />
+            <Screen name="players" component={Players} />
+        </Navigator>
+    )
+}
--- src/Screens/Players/index.tsx
+++ src/Screens/Players/index.tsx
@@ -1,4 +1,4 @@
-import { Container, HeaderList } from "./styles";
+import { Container, HeaderList, NumberOfPlayers } from "./styles";
 import { Header } from "../../components/Header";
 import { Destaques } from "../../components/Destaques";
 import { ButtonIcon } from "../../components/ButtonIcon";
@@ -7,10 +7,14 @@ import { Form } from "./styles";
 import { Filter } from "../../components/Filter";
 import { FlatList } from "react-native";
 import { useState } from "react";
+import { ListEmpty } from "../../components/ListEmpty";
+import { PlayerCard } from "../../components/PlayerCard";
+import { Button } from "../../components/Button";
 
 export function Players() {
 
     const [team, setTeam] = useState("Time A");
+    const [players, setPlayers] = useState([]);
     return (
         <Container>
             <Header showBackButton />
@@ -35,7 +39,27 @@ export function Players() {
             showsHorizontalScrollIndicator={false}
             contentContainerStyle={{ paddingRight: 24 }}
             />
+
+            <NumberOfPlayers>{players.length}</NumberOfPlayers>
             </HeaderList>
+            <FlatList
+            data={players}
+            keyExtractor={item => item}
+            renderItem={({item})=>(
+                <PlayerCard name={item} 
+                onRemove={() => {}} />
+            )}
+            ListEmptyComponent={() => (
+                <ListEmpty message="Nenhum jogador adicionado 😢" />
+            )}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={[
+                { paddingBottom: 100 },
+                players.length === 0 && { flex: 1 }
+            ]}
+            />
+            
+         <Button title="Remover Turma" type="SECONDARY" />
         </Container>
     )
 }
